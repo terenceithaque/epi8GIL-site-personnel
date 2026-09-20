a76b58eb60f4ef17d709cc49bfb4124208b725c5
Terence Le Thierry d'Ennequin <terence.lethierry@protonmail.com>
2026-09-20 15:40:44 +0200

    Construction des fichiers finaux dans un dossier output/

--- building/build.sh
+++ building/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-source_file=${1:?Fichier source manquant} # Fichier source
+#source_file=${1:?Fichier source manquant} # Fichier source
 
-echo $source_file
+#echo $source_file
 
 
 
@@ -17,49 +17,71 @@ function error {
 
 
 
-test -f $source || error  "{$source}: fichier inexistant"
+#test -f $source || error  "{$source}: fichier inexistant"
 test -f layout/before.html || error "layout/before.html: fichier inexistant"
 test -f layout/after.html || error "layout/after.html: fichier inexistant"
 
 
+html_files=$(ls input/ | grep ".html")
+css_files=$(ls input/ | grep ".css")
 
-# Code source du fichier
-source_code=$(cat $source_file)
-title=$(head -1 $source_file)
-
-echo "Titre du fichier source: $title"
-
+echo "Fichiers html: $html_files"
+echo "Fichiers css: $css_files"
 
 
-# Actualiser la date de modification du fichier
-date=$(date)
-echo $date
+for source_file in input/*.html; do
+        echo "Chemin relatif du fichier source: $source_file"
 
+        file_name="${source_file#input/}"
+      
+        echo "Construction du fichier $file_name"
+        # Code source du fichier
+        source_code=$(cat $source_file)
+        title=$(head -1 $source_file)
 
-
-#  Code source du fichier before.html
-before_code=$(cat layout/before.html | sed "s/##TITRE##/$title/g")
-
-# Code source du fichier after.html
-after_code=$(cat layout/after.html | sed "s/##DATEMODIF##/$date/g")
+        echo "Titre du fichier source: $title"
 
 
 
-# Code final du fichier HTML
-final_code="$before_code$source_code$after_code"
-echo $final_code
+        # Actualiser la date de modification du fichier
+        date=$(date)
+        #echo $date
+
+
+
+        #  Code source du fichier before.html
+        before_code=$(cat layout/before.html | sed "s/##TITRE##/$title/g")
+
+        # Code source du fichier after.html
+        after_code=$(cat layout/after.html | sed "s/##DATEMODIF##/$date/g")
+
+
+
+        # Code final du fichier HTML
+        final_code="$before_code$source_code$after_code"
+        #echo $final_code
 
 
 
 
 
-# Ecrire le code source complet dans le fichier final
-echo $final_code > "{$source_file}_final.html"
+        # Ecrire le code source complet dans le fichier final
+        echo $final_code > "output/$file_name"
+
+        echo "Fini de construire output/$file_name"
+done
 
 
-echo "final file created, opening..."
+for css_file in input/*.css; do
+
+        echo "Copie de la feuille de style:  ${css_file}"
+
+        file_name="${css_file/input}"
+
+        cp $css_file "output/$file_name"
+
+        echo "Copié la feuille de style dans output/$file_name"
 
 
-# Ouvrir le fichier
-xdg-open "{$source_file}_final.html" 
-
+        echo "Terminé la copie des fichiers dans output/"
+done
